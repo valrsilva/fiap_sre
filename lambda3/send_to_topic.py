65e4cca3fb493f1a6749d00e7e8cb279dd38d6f2
Valdeci <valdeci@plusoft.com.br>
2020-12-19 23:01:50 -0300

    xray

--- lambda3/send_to_topic.py
+++ lambda3/send_to_topic.py
@@ -1,5 +1,16 @@
-import json
+import sys
+sys.path.insert(0, '/opt')
+
 import boto3
+import json
+
+from aws_xray_sdk.core import xray_recorder
+from aws_xray_sdk.core import patch_all
+
+xray_recorder.configure(service='trabalho10')
+plugins = ('ElasticBeanstalkPlugin', 'EC2Plugin')
+xray_recorder.configure(plugins=plugins)
+patch_all()
 
 def lambda_handler(event, context):
     
